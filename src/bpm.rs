c8688d8c190c6ff59e89011b10bf94cfa4aff69c
Connor Tsui <connor.tsui20@gmail.com>
2024-03-27 23:53:47 -0400

    allocate frames up front

--- src/bpm.rs
+++ src/bpm.rs
@@ -1,28 +1,46 @@
-use std::{collections::HashMap, sync::Arc};
 use crate::{
     frame::Frame,
     io::IoUringAsync,
-    page::{page_handle::PageHandle, Page, PageId},
+    page::{page_handle::PageHandle, PageId, PageRef, PAGE_SIZE},
 };
 use crossbeam_queue::ArrayQueue;
-use tokio::sync::RwLock;
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::{Mutex, RwLock};
 
 /// A parallel Buffer Pool Manager that manages bringing logical pages from disk into memory via
 /// shared and fixed buffer frames.
+#[derive(Debug)]
 pub struct BufferPoolManager {
-    pub(crate) frames: Vec<Frame>,
+    pub(crate) active_pages: Mutex<Vec<PageRef>>,
     pub(crate) free_frames: ArrayQueue<Frame>,
-    pub(crate) pages: RwLock<HashMap<PageId, Arc<Page>>>,
+    pub(crate) pages: RwLock<HashMap<PageId, PageRef>>,
     num_frames: usize,
 }
 
 impl BufferPoolManager {
     /// Constructs a new buffer pool manager.
     pub fn new(num_frames: usize) -> Self {
-         // TODO register frames via IoSlice
+        // All frames start out as free
+        let free_frames = ArrayQueue::new(num_frames);
+
+        // Allocate all of the memory up front
+        let allocation = vec![0u8; num_frames * PAGE_SIZE];
+
+        let bytes: &'static mut [u8] = allocation.leak();
+        debug_assert_eq!(bytes.len(), num_frames * PAGE_SIZE);
+
+        // Note: use `as_chunks_unchecked_mut()` when it is stabilized
+        // https://doc.rust-lang.org/std/primitive.slice.html#method.as_chunks_unchecked_mut
+        for buf in bytes.chunks_exact_mut(PAGE_SIZE) {
+            let frame = Frame::new(buf);
+            free_frames
+                .push(frame)
+                .expect("Was not able to add the frame to the free_frames list");
+        }
+
         Self {
-            frames: (0..num_frames).map(|_| Frame::default()).collect(),
-            free_frames: ArrayQueue::new(num_frames),
+            active_pages: Mutex::new(Vec::with_capacity(num_frames)),
+            free_frames,
             pages: RwLock::new(HashMap::with_capacity(num_frames)),
             num_frames,
         }
