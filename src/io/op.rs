c95f1996ce10c8a611861e6d59d8d21299e4ad43
Connor Tsui <connor.tsui20@gmail.com>
2024-03-29 09:45:10 -0400

    move stuff around

--- src/io/op.rs
+++ src/io/op.rs
@@ -7,6 +7,7 @@ use std::future::Future;
 use std::rc::Rc;
 
 /// The `IoUring` lifecycle state.
+#[derive(Debug)]
 pub(super) enum Lifecycle {
     /// The operation has been entered onto the submission queue, but has not been submitted to the
     /// kernel via `io_uring_enter` by the asynchronous runtime.
@@ -25,10 +26,15 @@ pub(super) struct OpInner {
     pub(super) id: u64,
 }
 
-/// A wrapper around an optionally owned `OpInner` type.
-pub struct Op {
-    /// Ownership over the `OpInner` value is moved to a new tokio task when an `Op` is dropped.
-    pub(super) inner: Option<OpInner>,
+impl Drop for OpInner {
+    fn drop(&mut self) {
+        let mut guard = self.operations.borrow_mut();
+        let lifecycle = guard.remove(&self.id);
+
+        let Some(Lifecycle::Completed(_)) = &lifecycle else {
+            unreachable!("`OpInner` was dropped before completing its operation");
+        };
+    }
 }
 
 impl Future for OpInner {
@@ -59,14 +65,29 @@ impl Future for OpInner {
     }
 }
 
-impl Drop for OpInner {
-    fn drop(&mut self) {
-        let mut guard = self.operations.borrow_mut();
-        let lifecycle = guard.remove(&self.id);
+/// A wrapper around an optionally owned `OpInner` type.
+pub struct Op {
+    /// Ownership over the `OpInner` value is moved to a new tokio task when an `Op` is dropped.
+    pub(super) inner: Option<OpInner>,
+}
 
-        let Some(Lifecycle::Completed(_)) = &lifecycle else {
-            unreachable!("`OpInner` was dropped before completing its operation");
-        };
+/// If `Op` gets dropped before it has finished its operation, someone has to clean up.
+/// The inner future is spawned again as a task onto the current thread, where it will complete.
+impl Drop for Op {
+    fn drop(&mut self) {
+        // We only take the `OpInner` out once (here during `drop`), so this is safe to unwrap
+        let inner = self.inner.take().unwrap();
+        let guard = inner.operations.borrow();
+
+        // This is safe to unwrap since we only remove the `Lifecycle` from the table after the
+        // `OpInner` gets dropped, and that _must_ happen after this gets dropped.
+        match guard.get(&inner.id).unwrap() {
+            Lifecycle::Completed(_) => {}
+            _ => {
+                drop(guard);
+                tokio::task::spawn_local(inner);
+            }
+        }
     }
 }
 
@@ -80,24 +101,5 @@ impl Future for Op {
     ) -> std::task::Poll<Self::Output> {
         // `inner` is only `None` after we drop `Op`, so we can unwrap safely
         std::pin::Pin::new(self.inner.as_mut().unwrap()).poll(cx)
-    }
-}
-
-/// If `Op` gets dropped before it has finished its operation, someone has to clean up.
-/// The inner future is spawned again as a task onto the current thread, where it will complete.
-impl Drop for Op {
-    fn drop(&mut self) {
-        let inner = self.inner.take().unwrap();
-        let guard = inner.operations.borrow();
-
-        // This is safe to unwrap since we only remove the `Lifecycle` from the table after the
-        // `OpInner` gets dropped
-        match guard.get(&inner.id).unwrap() {
-            Lifecycle::Completed(_) => {}
-            _ => {
-                drop(guard);
-                tokio::task::spawn_local(inner);
-            }
-        }
     }
 }
