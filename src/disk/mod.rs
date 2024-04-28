2823293232ee95896553b8aa7cd8a48d6216f03e
Connor Tsui <connor.tsui20@gmail.com>
2024-04-27 20:09:26 -0400

    add module level docs for disk_manager

--- src/disk/mod.rs
+++ src/disk/mod.rs
@@ -1,3 +1,6 @@
+//! Implementation of functionality related to the management of data in between non-volatile /
+//! permanent / disk storage and memory.
+
 pub mod disk_manager;
 pub mod eviction;
 pub mod frame;
