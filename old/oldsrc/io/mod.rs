b1e4c48a3f1b74c48bc7213bf98918d2ac87ecf1
Connor Tsui <connor.tsui20@gmail.com>
2024-08-08 00:57:46 +0200

    remove old

--- old/oldsrc/io/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Implementation of I/O operations and functionality based on the linux `io_uring` interface.
-
-pub(crate) mod op;
-pub(crate) mod uring_async;
-
-pub use uring_async::IoUringAsync;
