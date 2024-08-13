a144c1d3f0ed822bc8566ffb97c510b4e2974771
Connor Tsui <connor.tsui20@gmail.com>
2024-08-13 09:30:37 +0200

    add zipfian to throughput bench and clean up

--- tests/throughput.rs
+++ tests/throughput.rs
@@ -9,63 +9,61 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering},
     thread,
 };
-use tokio::task::JoinSet;
-
-static COUNTER: AtomicUsize = AtomicUsize::new(0);
+use tokio::task::{JoinHandle, JoinSet};
+use zipf::ZipfDistribution;
 
 const GIGABYTE: usize = 1024 * 1024 * 1024;
 const GIGABYTE_PAGES: usize = GIGABYTE / PAGE_SIZE;
 
+const THREADS: usize = 4;
+const TASKS: usize = 64; // tasks per thread
+const OPERATIONS: usize = 1 << 22;
+
+const THREAD_OPERATIONS: usize = OPERATIONS / THREADS;
+const ITERATIONS: usize = THREAD_OPERATIONS / TASKS; // iterations per task
+
+const FRAMES: usize = GIGABYTE_PAGES;
+const STORAGE_PAGES: usize = 32 * GIGABYTE_PAGES;
+
+/// The 30% was taken from the Redshift paper "Why TPC Is Not Enough", Table 2.
+const WRITE_RATIO: f64 = 30.0 / 100.0;
+const ZIPF_EXP: f64 = 1.1;
+
+static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
 #[test]
-fn throughput() {
-    const THREADS: usize = 4;
-    const TASKS: usize = 64; // tasks per thread
-    const OPERATIONS: usize = 1 << 22;
+#[ignore]
+fn bench_random() {
+    throughput::<false>();
+}
 
-    const THREAD_OPERATIONS: usize = OPERATIONS / THREADS;
-    const ITERATIONS: usize = THREAD_OPERATIONS / TASKS; // iterations per task
+#[test]
+#[ignore]
+fn bench_zipf() {
+    throughput::<true>();
+}
 
-    const FRAMES: usize = GIGABYTE_PAGES;
-    const STORAGE_PAGES: usize = 32 * GIGABYTE_PAGES;
-
+/// Spawns a constant number of threads and schedules a constant number of tasks on each of them,
+/// with each task containing a constant number of read/write operations.
+///
+/// See [`spawn_bench_task`] for more information.
+fn throughput<const ZIPF: bool>() {
     BufferPoolManager::initialize(FRAMES, STORAGE_PAGES);
-    let bpm = BufferPoolManager::get();
 
-    let coin = Bernoulli::new(20.0 / 100.0).unwrap();
-
+    COUNTER.store(0, Ordering::Release);
     println!("Operations: {OPERATIONS}");
 
     let start = std::time::Instant::now();
 
-    // Spawn all threads
     thread::scope(|s| {
+        // Spawn all threads with tasks on them.
         for _thread in 0..THREADS {
             s.spawn(move || {
                 BufferPoolManager::start_thread(async move {
+                    // Spawn all of the tasks lazily.
                     let mut set = JoinSet::new();
                     for _task in 0..TASKS {
-                        let handle = BufferPoolManager::spawn_local(async move {
-                            let mut rng = rand::thread_rng();
-
-                            for _iteration in 0..ITERATIONS {
-                                let id = rng.gen_range(0..STORAGE_PAGES) as u64;
-                                let pid = PageId::new(id);
-                                let ph = bpm.get_page(&pid).await.unwrap();
-
-                                if coin.sample(&mut rng) {
-                                    let mut guard = ph.write().await;
-                                    guard.deref_mut().fill(b'a');
-                                    guard.flush().await.unwrap();
-                                } else {
-                                    let guard = ph.read().await;
-                                    let slice = guard.deref();
-                                    std::hint::black_box(slice);
-                                }
-
-                                COUNTER.fetch_add(1, Ordering::Release);
-                            }
-                        });
-
+                        let handle = spawn_bench_task::<ZIPF>();
                         set.spawn(handle);
                     }
 
@@ -78,6 +76,7 @@ fn throughput() {
             });
         }
 
+        // Spawn a counter thread that reports metrics every second.
         s.spawn(move || {
             let second = std::time::Duration::from_secs(1);
             let mut counter = 0;
@@ -99,4 +98,37 @@ fn throughput() {
     });
 
     assert_eq!(COUNTER.load(Ordering::SeqCst), THREADS * TASKS * ITERATIONS);
+}
+
+fn spawn_bench_task<const ZIPF: bool>() -> JoinHandle<()> {
+    let bpm = BufferPoolManager::get();
+
+    let zipf = ZipfDistribution::new(STORAGE_PAGES, ZIPF_EXP).unwrap();
+    let coin = Bernoulli::new(WRITE_RATIO).unwrap();
+    let mut rng = rand::thread_rng();
+
+    BufferPoolManager::spawn_local(async move {
+        for _iteration in 0..ITERATIONS {
+            let id = if ZIPF {
+                zipf.sample(&mut rng)
+            } else {
+                rng.gen_range(0..STORAGE_PAGES)
+            } as u64;
+
+            let pid = PageId::new(id);
+            let ph = bpm.get_page(&pid).await.unwrap();
+
+            if coin.sample(&mut rng) {
+                let mut write_guard = ph.write().await;
+                write_guard.deref_mut().fill(b'a');
+                write_guard.flush().await.unwrap();
+            } else {
+                let read_guard = ph.read().await;
+                let slice = read_guard.deref();
+                std::hint::black_box(slice);
+            }
+
+            COUNTER.fetch_add(1, Ordering::Release);
+        }
+    })
 }
