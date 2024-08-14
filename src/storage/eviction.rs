d2369118288839d25d77a57f8865041e2257a772
Connor Tsui <connor.tsui20@gmail.com>
2024-08-14 20:50:36 +0200

    remove eviction file

--- src/storage/eviction.rs
+++
@@ -1,4 +0,0 @@
-//! This module contains the type definiton of [`EvictionState`], which is used to manage per-frame
-//! state used inside the buffer pool's eviction algorithm.
-//!
-//! TODO more docs.
