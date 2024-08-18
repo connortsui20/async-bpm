15cff3addd8b8440ea410b6e017dabd23b720613
Connor Tsui <connor.tsui20@gmail.com>
2024-08-18 17:25:56 -0400

    first async traditional commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
-#![doc = include_str!("../README.md")]
-#![cfg(target_family = "unix")]
-#![warn(missing_docs)]
-#![warn(clippy::missing_docs_in_private_items)]
-#![warn(clippy::missing_errors_doc)]
-#![warn(clippy::missing_panics_doc)]
-#![warn(clippy::missing_safety_doc)]
-
 mod bpm;
-pub mod page;
-pub(crate) mod storage;
+mod replacer;
 
-pub use bpm::BufferPoolManager;
+pub use bpm::*;
